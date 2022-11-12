544637b10d38a3b02edf9e5f68de4a4e6458a7fb
Cliqin <104069156+Cliqin@users.noreply.github.com>
2022-11-12 14:15:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,117 +1,188 @@
+import json
+import jsonpath
+import requests
 import os
 import sys
-import time
 import traceback
-from selenium import webdriver
-import requests
-import selenium.webdriver
 from loguru import logger
-from selenium.webdriver.chrome.options import Options
+
+import datetime
+import time
+
+import selenium.common
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.relative_locator import locate_with
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.relative_locator import locate_with
+
+# 周次表示
+weeks = {
+    1: 1,
+    2: 2,
+    3: 4,
+    4: 8,
+    5: 16,
+    6: 32,
+    7: 64,
+    8: 128,
+    9: 256,
+    10: 512,
+    11: 1024,
+    12: 2048,
+    13: 4096,
+    14: 8192,
+    15: 16384,
+    16: 32768,
+    17: 65536,
+    18: 131072,
+    20: 262144,
+    21: 524288,
+}
+# 星期几
+week_list = [1, 2, 3, 4, 5, 6, 7]
+# 节次字典
+times_dic = {
+    '上午12': 3,
+    '上午34': 12,
+    '下午56': 48,
+    '下午78': 192,
+    '晚上910': 1792
+}
+# 节次列表
+times_list = ['上午12', '上午34', '下午56', '下午78', '晚上910']
 
 
-class Free:
+def sorting(dic):
+    # obj = json.load(open('result.json', 'r', encoding='utf-8'))
+    # course_list = jsonpath.jsonpath(obj, '$..cdmc')
+    course_list = []
+    listing = json.loads(dic)['items']
+    for i in listing:
+        course_list.append(i['cdmc'])
+    return course_list
 
+
+class Normal:
     def __init__(self):
-        self.xuhao = str("32106100117")
-        self.mima = str('Aa18319093951!')
-        # self.pushplus = str('da9840d244194425bb1d1435fcd662da')
+        self.last_list = {}
+        self.final = ''
 
-        path = 'chromedriver.exe'
-        self.driver = webdriver.Chrome(path)
-        self.wdwait = WebDriverWait(self.driver, 30)
-        # self.driver.get('https://baidu.com')
+        self.pushplus = ['da9840d244194425bb1d1435fcd662da']
+
+        timing = datetime.datetime.now()
+
+        self.url = 'http://jwxt.gzhu.edu.cn/jwglxt/cdjy/cdjy_cxKxcdlb.html?doType=query&gnmkdm=N2155'
+        self.headers = {
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'zh-CN,zh;q=0.9,en-GB;q=0.8,en;q=0.7,en-US;q=0.6',
+            'Connection': 'keep-alive',
+            'Content-Length': '249',
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+            'Cookie': '',
+            'Host': 'jwxt.gzhu.edu.cn',
+            'Origin': 'http://jwxt.gzhu.edu.cn',
+            'Referer': 'http://jwxt.gzhu.edu.cn/jwglxt/cdjy/cdjy_cxKxcdlb.html?gnmkdm=N2155&layout=default&su=32106100117',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35',
+            'X-Requested-With': 'XMLHttpRequest'
+        }
+        self.data = {
+            'fwzt': 'cx',
+            'xqh_id': '1',  # 校区
+            'xnm': '2022',
+            'xqm': '3',
+            'cdlb_id': '11',
+            # 'cdejlb_id': '',
+            # 'qszws': '',
+            # 'jszws': '',
+            # 'cdmc': '',
+            'lh': '6',  # 楼号
+            'jyfs': '0',
+            # 'cdjylx': '',
+            'zcd': '',  # 周次 二进制
+            'xqj': '',  # 星期几
+            'jcd': '',  # 节次 二进制
+            '_search': 'false',
+            'nd': 1668063026809,
+            'queryModel.showCount': '200',
+            # 'queryModel.currentPage': '1',
+            # 'queryModel.sortName': 'cdbh',
+            # 'queryModel.sortOrder': 'asc',
+            # 'time': '2'
+        }
 
         self.page = 0
-        self.fail = False
+        self.flag = False
 
-    def __call__(self):
-        # 总共执行5次
-        for retries in range(5):
-            try:
-                logger.info(f"第{retries + 1}次运行")
-                if retries:
-                    self.refresh()
+        # 星期几
+        self.realweekday = (week_list[timing.weekday()])
+        self.data['xqj'] = self.realweekday
+        # 录入周次
+        fp = open('weeklog.txt', 'r', encoding='utf-8')
+        self.weeking = int(fp.read())
+        fp.close()
+        self.data['zcd'] = weeks[self.weeking]
+        # 并判断是否调整周次
+        if int(week_list[timing.weekday()]) == 7:
+            fp = open('weeklog.txt', 'w', encoding='utf-8')
+            fp.write(str(self.weeking + 1))
+            fp.close()
 
-                if self.page == 0:
-                    # 登陆操作
-                    self.step1()
+    def steps(self, cookies):
+        logger.info('输入cookies')
+        self.headers['Cookie'] = cookies
+        # 放入节次循环中
+        logger.info('进入查询')
+        for i in times_list:
+            self.data['jcd'] = times_dic[i]
+            print(self.data['zcd'], self.data['xqj'], self.data['jcd'])
 
-                if self.page in [0, 1]:
-                    self.step2()
+            response = requests.post(self.url, headers=self.headers, data=self.data)
 
-                    break
-            except Exception:
-                logger.error(traceback.format_exc())
-                logger.error(f'第{retries + 1}次运行失败，当前页面标题为：{self.driver.title}')
-                if retries == 4:
-                    self.fail = True
+            # 摒弃依靠写入文件的存储模式
+            # logger.info('打开文件写入')
+            # with open('result.json', 'w', encoding='utf‐8') as fp:
+            #     fp.write(response.text)
+            # logger.info('正在解析中')
 
-    def refresh(self):
-        refresh_times = 0
-        while True:
-            logger.info('刷新页面')
-            self.driver.refresh()
+            self.last_list[i] = sorting(response.text)
 
-            title = self.driver.title
-            if title == '融合门户':
-                self.page = 1
-            elif title == '学生健康状况申报':
-                self.page = 2
-            elif title in ['填报健康信息 - 学生健康状况申报', '表单填写与审批::加载中']:
-                self.page = 3
-            elif title == "":
-                logger.info('当前页面标题为：')
+        self.flag = True
 
-                refresh_times += 1
-                if refresh_times < 4:
-                    continue
+    def output(self):
+        print('星期', self.realweekday)
+        print('************\n\n')
+
+        logger.info('发送信息中')
+        if self.flag:
+            for i in times_list:
+                self.final = self.final + str(i) + '\n' + str(self.last_list[i]) + '\n\n'
+                print(i)
+                print(self.last_list[i])
+                print('************\n\n')
+
+            if self.pushplus:
+                for i in self.pushplus:
+                    tim = datetime.datetime.now()
+                    data = {"token": i, "title": f'{tim.month}月{tim.day}号空教室', "content": self.final + '龙哥,教我java'}
+                    url = "http://www.pushplus.plus/send/"
+                    logger.info(requests.post(url, data=data, timeout=10).text)
             else:
-                self.page = 0
+                logger.error('pushplus失效')
+        else:
+            if self.pushplus:
+                for i in self.pushplus:
+                    data = {"token": i, "title": '空教室查询失败',
+                            "content": f'{logger.error(traceback.format_exc())}'}
+                    url = "http://www.pushplus.plus/send/"
+                    logger.info(requests.post(url, data=data, timeout=10).text)
+            else:
+                logger.error('pushplus失效')
 
-            break
-
-        logger.info(f'当前页面标题为：{title}')
-
-    # 登陆操作
-    def step1(self):
-        logger.info('正在转到统一身份认证页面')
-        self.driver.get(
-            'https://newcas.gzhu.edu.cn/cas/login?service=https%3A%2F%2Fnewmy.gzhu.edu.cn%2Fup%2Fview%3Fm%3Dup'
-        )
-
-        self.wdwait.until(
-            EC.visibility_of_element_located(
-                (By.XPATH, "//div[@class='robot-mag-win small-big-small']")))
-
-        logger.info('正在尝试登陆融合门户')
-        for script in [
-            f"document.getElementById('un').value='{self.xuhao}'",
-            f"document.getElementById('pd').value='{self.mima}'",
-            "document.getElementById('index_login_btn').click()"
-        ]:
-            self.driver.execute_script(script)
-        print('**************', self.driver.title)
-
-    def step2(self):
-        self.wdwait.until(
-            EC.visibility_of_element_located(
-                (By.XPATH, '//a[@title="教务系统"]/img')))
-
-        logger.info('正在转到教务系统')
-        self.driver.find_elements(by=By.XPATH, value='//a[@title="教务系统"]')[0].click()
-        # print(self.driver.get_cookies())
-        # time.sleep(80)
-        # # self.wdwait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="btn_yd"]')))
-        # # self.driver.find_elements(by=By.XPATH, value='//*[@id="btn_yd"]')[0].click()
-        # self.driver.get('http://jwxt.gzhu.edu.cn/jwglxt/xtgl/index_initMenu.html?jsdm=&_t=1668059002212')
-        # time.sleep(5)
-        # print('**************', self.driver.title)
-        print(self.driver.get_cookie())
 
 if __name__ == '__main__':
-    f = Free()
-    f()
+    a = Normal()
+    a.steps('JSESSIONID=9167C91EE5BA6D069FAA218B3A6C6170')
+    a.output()
